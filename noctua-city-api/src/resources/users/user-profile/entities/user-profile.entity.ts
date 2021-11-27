841250c4a5b7cf02a305938cea980b1ad941e7a7
JACQUENET Christophe <jjacquenet@myges.fr>
2021-11-27 15:09:10 +0100

    save

--- noctua-city-api/src/resources/users/user-profile/entities/user-profile.entity.ts
+++ noctua-city-api/src/resources/users/user-profile/entities/user-profile.entity.ts
@@ -1,1 +1,11 @@
-export class UserProfile {}
+import { IncrementalDatetimeEntityAbstract } from '../../../common/abstract/incremental-datetime-entity.abstract';
+import { Entity, OneToOne } from '@mikro-orm/core';
+import { User } from '../../user/entities/user.entity';
+
+@Entity()
+export class UserProfile extends IncrementalDatetimeEntityAbstract{
+
+    @OneToOne(() => User, user => user.profile)
+    user: string;
+    
+}
--- noctua-city-api/src/resources/users/user/entities/user.entity.ts
+++ noctua-city-api/src/resources/users/user/entities/user.entity.ts
@@ -1,8 +1,18 @@
-import { Entity } from '@mikro-orm/core';
+import { Entity, OneToOne } from '@mikro-orm/core';
 import { UuidEntityAbstract } from '../../../common/abstract/uuid-entity.abstract';
+import { UserProfile } from '../../user-profile/entities/user-profile.entity';
 
 @Entity()
-export class User extends UuidEntityAbstract implements {
+export class User extends UuidEntityAbstract {
 
+    email: string;
+    firstname: string;
+    lastname: string;
+    password: string;
+    dateOfBirth: Date;
+    agreedTermAndCondition: boolean;
+    
+    @OneToOne(() => UserProfile, profile => profile.user, { owner: true, orphanRemoval: true})
+    profile!: UserProfile;
 
 }
